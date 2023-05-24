457e1d91692f727b126b135e47c1bb15bf0fb2e3
behnam <behnamtakta71@gmail.com>
2023-05-24 20:38:36 +0430

    Binding was done in the person service provider

--- module/person/Providers/PersonServiceProvider.php
+++ module/person/Providers/PersonServiceProvider.php
@@ -4,12 +4,29 @@ namespace PERSON\Providers;
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use PERSON\Repository\v1\PersonRepository;
+use PERSON\Repository\v1\PersonRepositoryInterface;
+use PERSON\Service\v1\PersonService;
+use PERSON\Service\v1\PersonServiceInterface;
 
 class PersonServiceProvider extends ServiceProvider
 {
     public function boot()
     {
         $this->router();
+    }
+
+    public function register()
+    {
+        $this->app->bind(
+            PersonRepositoryInterface::class,
+            PersonRepository::class
+        );
+
+        $this->app->bind(
+            PersonServiceInterface::class,
+            PersonService::class
+        );
     }
 
     public function router(): void
