7bdc6e606ac50d7c9e98e79e81fdb27f6fd33255
behnam <behnamtakta71@gmail.com>
2023-05-25 12:37:35 +0430

    Write calculation methods

--- module/invoice/Service/v1/InvoiceService.php
+++ module/invoice/Service/v1/InvoiceService.php
@@ -16,28 +16,80 @@ class InvoiceService implements InvoiceServiceInterface
         $this->invoiceRepository = $invoiceRepository;
     }
 
-    public function getInvoice(int $id): ?Invoice
-    {
-        // TODO: Implement getInvoice() method.
-    }
-
     public function getAllInvoices()
     {
-        // TODO: Implement getAllInvoices() method.
+        return $this->invoiceRepository->getAll();
+    }
+
+    public function getInvoice(int $id): ?Invoice
+    {
+        return $this->invoiceRepository->getById($id);
     }
 
     public function createInvoice(array $data): ?Invoice
     {
-        // TODO: Implement createInvoice() method.
+        // Validation and business logic
+        // Calculate amount, total after discount, tax, etc.
+
+        $invoice = $this->invoiceRepository->create($data);
+
+        // Fire the event
+//        event(new InvoiceCreated($invoice));
+
+        return $invoice;
     }
 
     public function updateInvoice(int $id, array $data): ?Invoice
     {
-        // TODO: Implement updateInvoice() method.
+        // Validation and business logic
+        // Calculate amount, total after discount, tax, etc.
+
+        $invoice = $this->invoiceRepository->update($id, $data);
+
+        // Fire the event
+//        event(new InvoiceUpdated($invoice));
+
+        return $invoice;
     }
 
     public function deleteInvoice(int $id): bool
     {
-        // TODO: Implement deleteInvoice() method.
+        $invoice = $this->invoiceRepository->getById($id);
+
+        $this->invoiceRepository->delete($id);
+
+        // Fire the event
+//        event(new InvoiceDeleted($invoice));
+    }
+
+
+    protected function calculateAmount(float $quantity, int $price): int
+    {
+        return (int) ($quantity * $price);
+    }
+
+    protected function calculateDiscount(float $amount, int $discount) :int
+    {
+        return (int) ($amount * $discount  / 100 );
+    }
+
+    protected function calculateTotalAfterDiscount(int $amount, int $discount): int
+    {
+        return $amount - $discount;
+    }
+
+    protected function calculateTax(int $totalAfterDiscount, int $tax): int
+    {
+        return $totalAfterDiscount * $tax / 100;
+    }
+
+    protected function calculateTotalDue(int $totalAfterDiscount, int $tax): int
+    {
+        return $totalAfterDiscount + $tax;
+    }
+
+    protected function calculateTotalSum(array $amounts): int
+    {
+        return array_sum($amounts);
     }
 }
