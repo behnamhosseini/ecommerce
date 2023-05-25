993f7105c4c96b9e60f9c7954a1ee61dbe74a963
behnam <behnamtakta71@gmail.com>
2023-05-25 08:57:41 +0430

    commit moved

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,11 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -21,10 +25,45 @@ class Handler extends ExceptionHandler
     /**
      * Register the exception handling callbacks for the application.
      */
-    public function register(): void
+    public function register()
     {
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e)
+    {
+        if (!$request->expectsJson()) {
+            return parent::render($request, $e);
+        }
+        switch (true) {
+            case $e instanceof ModelNotFoundException:
+                return response()->json([
+                    'message' => 'Record not found',
+                ], 404);
+                break;
+            case $e instanceof NotFoundHttpException:
+                return response()->json([
+                    'message' => 'Page not found',
+                ], 404);
+                break;
+            case $e instanceof AuthenticationException:
+                return response()->json([
+                    'message' => 'Unauthenticated',
+                ], 401);
+                break;
+            case $e instanceof ValidationException:
+                return response()->json([
+                    'message' => 'Validation failed',
+                    'errors' => $e->errors(),
+                ], 400);
+                break;
+            default:
+                return response()->json([
+                    'message' => 'Internal Server Error',
+                ], 500);
+                break;
+        }
     }
 }
